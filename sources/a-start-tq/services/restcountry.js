3bb1000ab3735ed77b76a558708835dbab6efef5
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-07-09 15:01:10 +0700

    fix: new endpoint for restcountries

--- sources/a-start-tq/services/restcountry.js
+++ sources/a-start-tq/services/restcountry.js
@@ -1,7 +1,9 @@
 const BASE_URL = "https://restcountries.com/v3.1";
 
 const getAllCountries = async () => {
-  const response = await fetch(`${BASE_URL}/all`);
+  const response = await fetch(
+    `${BASE_URL}/all?fields=capital,flags,latlng,name,region,subregion`
+  );
   const responseJson = await response.json();
 
   return responseJson;
